79051e97634474070136bee7c25768a6b927db3f
Umpoy <iumpoy@yahoo.com>
2018-10-26 10:38:08 -0700

    set up cookie-parser and express-session

--- server/app.js
+++ server/app.js
@@ -2,9 +2,19 @@ const express = require('express');
 const app = express();
 const port = 5000;
 
+const cookieParser = require('cookie-parser');
+const session = require('express-session');
 const morgan = require('morgan');
 
+
+
 app.use(morgan('dev'));
+app.use(cookieParser());
+app.use(session({
+    secret: 'string',
+    saveUninitialized: true,
+    resave: true
+}));
 
 app.get('/', (req, res) => {
     res.send('Hello World');
