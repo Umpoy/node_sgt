248065442eeab3625c01afaa5dd92da6523c25fa
Umpoy <iumpoy@yahoo.com>
2018-10-26 10:52:30 -0700

    setup mysql and connected to database

--- server/app.js
+++ server/app.js
@@ -1,11 +1,21 @@
 const express = require('express');
 const app = express();
+const mysql = require('mysql');
 const port = 5000;
 
 const cookieParser = require('cookie-parser');
 const session = require('express-session');
 const morgan = require('morgan');
 
+const configDB = require('./config/database');
+const db = mysql.createConnection(configDB);
+
+db.connect(function (err) {
+    if (err) {
+        throw err
+    };
+    console.log("Connected!");
+});
 
 
 app.use(morgan('dev'));
