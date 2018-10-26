9bec0df6c530ddb742289a24818345185b322645
Umpoy <iumpoy@yahoo.com>
2018-10-26 10:33:24 -0700

    set up morgan

--- server/app.js
+++ server/app.js
@@ -2,6 +2,10 @@ const express = require('express');
 const app = express();
 const port = 5000;
 
+const morgan = require('morgan');
+
+app.use(morgan('dev'));
+
 app.get('/', (req, res) => {
     res.send('Hello World');
 });
