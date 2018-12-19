40302928c716e678439574c25d9b277a2bf67558
Umpoy <iumpoy@yahoo.com>
2018-12-18 17:07:20 -0800

    moved froms from old branch

--- server/app.js
+++ server/app.js
@@ -1,39 +1,32 @@
 const express = require('express');
 const app = express();
+const cors = require('cors');
 const mysql = require('mysql');
 const port = 5000;
 
 const cookieParser = require('cookie-parser');
 const session = require('express-session');
-const morgan = require('morgan'); const morgan = require('morgan');
+const morgan = require('morgan');
+const passport = require('passport');
+const flash = require('connect-flash');
 
 const configDB = require('./config/database');
 const db = mysql.createConnection(configDB);
 
-db.connect(function (err) {
-    if (err) {
-        throw err
-    };
-    console.log("Connected!");
-});
-
-db.query("SELECT * FROM customers", (err, result, fields) => {
-    if (err) {
-        throw err;
-    }
-    console.log(result);
-})
-
-
-app.use(morgan('dev')); app.use(morgan('dev'));
+app.use(cors());
+app.use(express.json());
+app.use(morgan('dev'));
 app.use(cookieParser());
 app.use(session({
     secret: 'string',
     saveUninitialized: true,
     resave: true
 }));
+app.use(passport.initialize());
+app.use(passport.session());
+app.use(flash());
 
-require('./app/routes.js')(app);
+require('./app/routes.js')(app, db);
 
 app.listen(port, () => {
     console.log("Server started in port " + port);
