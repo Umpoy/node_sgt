8309164c83114736ddaccfb32096ba265f9b59dc
Umpoy <iumpoy@yahoo.com>
2018-12-18 16:59:19 -0800

    staching commit

---
+++ server/app.js
@@ -0,0 +1,40 @@
+const express = require('express');
+const app = express();
+const mysql = require('mysql');
+const port = 5000;
+
+const cookieParser = require('cookie-parser');
+const session = require('express-session');
+const morgan = require('morgan'); const morgan = require('morgan');
+
+const configDB = require('./config/database');
+const db = mysql.createConnection(configDB);
+
+db.connect(function (err) {
+    if (err) {
+        throw err
+    };
+    console.log("Connected!");
+});
+
+db.query("SELECT * FROM customers", (err, result, fields) => {
+    if (err) {
+        throw err;
+    }
+    console.log(result);
+})
+
+
+app.use(morgan('dev')); app.use(morgan('dev'));
+app.use(cookieParser());
+app.use(session({
+    secret: 'string',
+    saveUninitialized: true,
+    resave: true
+}));
+
+require('./app/routes.js')(app);
+
+app.listen(port, () => {
+    console.log("Server started in port " + port);
+});
--- server/config/index.js
+++
@@ -1,7 +0,0 @@
-const configValues = require('./config');
-
-module.exports = {
-    getDbConnectionString: () => {
-        return 'mongodb://' + configValues.uname + ':' + configValues.pwd + '@ds139334.mlab.com:39334/node_sgt'
-    }
-}
