0680bd10d9b205b0770c51e48e767e7003e2b575
Umpoy <iumpoy@yahoo.com>
2019-01-22 10:05:16 -0800

    created connection to db

--- server/index.js
+++ server/index.js
@@ -1,7 +1,11 @@
 const express = require('express');
 const app = express();
-
+const cors = require('cors');
 const port = 5000;
+
+const mysql = require('mysql');
+const configDB = require('./config/database');
+const db = mysql.createConnection(configDB)
 
 app.listen(port, () => {
     console.log("Server running on localhost: " + port);
