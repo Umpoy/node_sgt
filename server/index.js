aab6f9fc046e042e605c2ce4b951350d4385fccf
Umpoy <iumpoy@yahoo.com>
2019-01-22 10:07:34 -0800

    created first route

--- server/index.js
+++ server/index.js
@@ -5,7 +5,9 @@ const port = 5000;
 
 const mysql = require('mysql');
 const configDB = require('./config/database');
-const db = mysql.createConnection(configDB)
+const db = mysql.createConnection(configDB);
+
+require('./app/routes.js')(app, db);
 
 app.listen(port, () => {
     console.log("Server running on localhost: " + port);
